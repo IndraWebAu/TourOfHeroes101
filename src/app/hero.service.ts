8e892540cad637ba57264a9622831ed350e38dfe
Ant <hark777@hotmail.com>
2023-07-31 15:37:50 +1000

    use-http-to-get-data

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,24 +3,30 @@ import { Hero } from './interfaces/hero';
 import { Heroes } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messagesService: MessageService) { }
+  private heroUrl = 'api/heroes';
 
-  getHeroes = (): Observable<Hero[]> => {
-    const heroes = of(Heroes);
-    this.messagesService.add('Heroes Service: Fetched heroes');
-    return heroes;
-  }
+  constructor(
+    private messagesService: MessageService,
+    private http: HttpClient) { }
+
+  getHeroes = (): Observable<Hero[]> =>
+    this.http
+      .get<Hero[]>(this.heroUrl);
 
   getHero(id: number): Observable<Hero> {
     const hero = Heroes.find(h => h.id == id)!;
-    this.messagesService.add(`Hero retrieved: id=${id}`);
+    this.log(`Hero retrieved: id=${id}`);
     return of(hero);
   }
 
+  private log = (message: string) =>
+    this.messagesService
+      .add(`Message serve: ${message}`);
 }
